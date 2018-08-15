090f5b6cf4f22d3315035d99a0f31cb4f0831753
Mick Letofsky <mick.tosk@gmail.com>
2018-08-15 14:45:21 -0600

    Add files via upload

---
+++ Powershell/Install Trend Micro on Remote PCs.ps1
@@ -0,0 +1,11 @@
+﻿<##############################################################################
+ Created By:   Mick Letofsky
+ Create Date:  2018.08.15
+ Creation:     Script to install TrendMicro on remote PC
+##############################################################################>
+
+Invoke-Command -ComputerName AzureRemote01 -ScriptBlock { Start-Process "C:\ProgramData\WFBS-SVC_Agent_Installer.msi" -wait}
+Invoke-Command -ComputerName AzureRemote02 -ScriptBlock { Start-Process "C:\ProgramData\WFBS-SVC_Agent_Installer.msi" -wait}
+Invoke-Command -ComputerName AzureRemote03 -ScriptBlock { Start-Process "C:\ProgramData\WFBS-SVC_Agent_Installer.msi" -wait}
+
+
